a82ccb7d529faa08774623917f600d472ff8468b
romit5075 <romitdobariya5075@gmail.com>
2024-02-06 16:06:39 +0530

    first

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,16 +1,43 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
 })
+
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  constructor() {}
+  constructor() { }
 
+  foods = [
+    {
+      id: 1,
+      name: 'Apples',
+      type: 'fruit',
+    },
+    {
+      id: 2,
+      name: 'Carrots',
+      type: 'vegetable',
+    },
+    {
+      id: 3,
+      name: 'Cupcakes',
+      type: 'dessert',
+    },
+  ];
+
+  // compareWith(o1, o2) {
+  //   return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  // }
+
+  // handleChange(ev) {
+  //   console.log('Current value:', JSON.stringify(ev.target.value));
+  // }
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
   }
